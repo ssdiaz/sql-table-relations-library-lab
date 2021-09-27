d7e3de0ee31f5a303511c6f7087ff37233ca51ab
Samantha Diaz <samidiaz3@gmail.com>
2021-09-27 14:48:30 -0400

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,9 @@
+-- SELECT id
+-- AS last_id 
+-- FROM characters
+-- ORDER BY id DESC 
+-- LIMIT 1;
+
+-- UPDATE characters SET species = "Martians" WHERE id = last_id;
+
+UPDATE characters SET species = "Martian" WHERE id = 8;
